481bd211f89a3ff5f17ab86d48ac2c1de7d3359a
dfg-98 <dariofg98@gmail.com>
2023-04-26 17:11:58 -0400

    feat(apps): autodiscover modules on app loading

--- dj_rest_admin/__init__.py
+++ dj_rest_admin/__init__.py
@@ -1,3 +1,12 @@
-from .sites import site
+from django.utils.module_loading import autodiscover_modules
+
 from .decorators import register
 from .restmodeladmin import RestModelAdmin
+from .sites import AdminSite as RestAdminSite
+from .sites import site
+
+__all__ = ["site", "register", "RestAdminSite", "RestModelAdmin", "autodiscover"]
+
+
+def autodiscover():
+    autodiscover_modules("rest_admin", register_to=site)
--- dj_rest_admin/apps.py
+++ dj_rest_admin/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
-class RestadminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restadmin'
+class DjRestAdminAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dj_rest_admin"
+
+    def ready(self) -> None:
+        super().ready()
+        self.module.autodiscover()
