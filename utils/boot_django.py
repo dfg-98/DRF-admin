30096cf462fed559f86f7610ba26a961238fae3e
dfg-98 <dariofg98@gmail.com>
2023-04-26 18:12:03 -0400

    update LICENSE copyright and fix tests

--- utils/boot_django.py
+++ utils/boot_django.py
@@ -3,6 +3,7 @@
 # This file sets up and configures Django. It's used by scripts that need to
 # execute as if running in a Django server.
 import os
+
 import django
 from django.conf import settings
 
@@ -20,58 +21,56 @@ def boot_django():
             }
         },
         INSTALLED_APPS=(
-            'django.contrib.admin',
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.messages',
-            'django.contrib.staticfiles',
-            "restadmin",
+            "django.contrib.admin",
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.sessions",
+            "django.contrib.messages",
+            "django.contrib.staticfiles",
+            "dj_rest_admin",
             "rest_framework",
-            "tests"
+            "tests",
         ),
         TIME_ZONE="UTC",
         USE_TZ=True,
-
         MIDDLEWARE=[
-            'django.middleware.security.SecurityMiddleware',
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.middleware.common.CommonMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            "django.middleware.security.SecurityMiddleware",
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.middleware.common.CommonMiddleware",
+            "django.middleware.csrf.CsrfViewMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "django.contrib.messages.middleware.MessageMiddleware",
+            "django.middleware.clickjacking.XFrameOptionsMiddleware",
         ],
-        SECRET_KEY='django-insecure-test-key',
+        SECRET_KEY="django-insecure-test-key",
         REST_FRAMEWORK={
-            'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-            'TEST_REQUEST_RENDERER_CLASSES': [
-                'rest_framework.renderers.MultiPartRenderer',
-                'rest_framework.renderers.JSONRenderer',
-                'rest_framework.renderers.TemplateHTMLRenderer', ],
+            "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+            "TEST_REQUEST_RENDERER_CLASSES": [
+                "rest_framework.renderers.MultiPartRenderer",
+                "rest_framework.renderers.JSONRenderer",
+                "rest_framework.renderers.TemplateHTMLRenderer",
+            ],
             "DEFAULT_AUTHENTICATION_CLASSES": [
-                'rest_framework.authentication.SessionAuthentication',
-                'rest_framework.authentication.BasicAuthentication'
+                "rest_framework.authentication.SessionAuthentication",
+                "rest_framework.authentication.BasicAuthentication",
             ],
             "DEFAULT_PERMISSION_CLASSES": [
-                'rest_framework.permissions.AllowAny',
-            ]
+                "rest_framework.permissions.AllowAny",
+            ],
         },
-
-        DEFAULT_AUTO_FIELD='django.db.models.BigAutoField',
-
-        STATIC_URL='static/',
+        DEFAULT_AUTO_FIELD="django.db.models.BigAutoField",
+        STATIC_URL="static/",
         TEMPLATES=[
             {
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [],
-                'APP_DIRS': True,
-                'OPTIONS': {
-                    'context_processors': [
-                        'django.template.context_processors.debug',
-                        'django.template.context_processors.request',
-                        'django.contrib.auth.context_processors.auth',
-                        'django.contrib.messages.context_processors.messages',
+                "BACKEND": "django.template.backends.django.DjangoTemplates",
+                "DIRS": [],
+                "APP_DIRS": True,
+                "OPTIONS": {
+                    "context_processors": [
+                        "django.template.context_processors.debug",
+                        "django.template.context_processors.request",
+                        "django.contrib.auth.context_processors.auth",
+                        "django.contrib.messages.context_processors.messages",
                     ],
                 },
             },
